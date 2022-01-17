d6e9d5e1cf6667163011a4985b062d893aed2931
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-01-17 23:14:51 +0300

    исправить замечания 1

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const profileOpenPopupButton = document.querySelector('.profile__edit-btn');
+const popup = document.querySelector('.popup');
+const form = document.querySelector('.form');
+const popupCloseButton = document.querySelector('.popup__close-btn');
+const formSubmitButton = document.querySelector('.form__submit-btn');
+const profileName = document.querySelector('.profile__name')
+const profileOccupation = document.querySelector('.profile__occupation')
+const formFieldName = document.querySelector('.form__field_type_name')
+const formFieldOccupation = document.querySelector('.form__field_type_occupation')
+
+
+function openPopup() {
+  formFieldName.value = profileName.textContent;
+  formFieldOccupation.value = profileOccupation.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+  profileName.textContent = formFieldName.value;
+  profileOccupation.textContent = formFieldOccupation.value;
+  closePopup();
+}
+
+profileOpenPopupButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+form.addEventListener('submit', formSubmitHandler);
+
