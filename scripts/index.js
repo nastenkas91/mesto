44f447c967a2f19b222227d75437cac78bce84e1
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-01-25 16:21:58 +0300

    добавлены карточки из коробки

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const profileOpenPopupButton = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
 const form = document.querySelector('.form');
@@ -6,6 +33,21 @@ const profileName = document.querySelector('.profile__name')
 const profileOccupation = document.querySelector('.profile__occupation')
 const formFieldName = document.querySelector('.form__field_type_name')
 const formFieldOccupation = document.querySelector('.form__field_type_occupation')
+
+//функция добавления карточки в разметку (в начало)
+function addCard(element) {
+  const cardList = document.querySelector('.elements__list');
+  const cardTemplate = document.querySelector('#placeCard').content;
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__image').src = element.link;
+  cardElement.querySelector('.element__image').alt = element.name;
+  cardElement.querySelector('.element__name').textContent = element.name;
+  cardList.prepend(cardElement);
+}
+
+//добавить карточки "из коробки"
+initialCards.forEach(addCard);
+
 
 
 function openPopup() {
