8d0f7100230ad0b2a6530d1fb5967ccd04eabecb
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-02-10 19:32:58 +0300

    рефакторинг

--- scripts/validate.js
+++ scripts/validate.js
@@ -48,7 +48,7 @@ const isValid = (formElement, inputElement, validationConfig) => {
     showInputError(formElement, inputElement, inputElement.validationMessage, validationConfig);
   }
   else {
-    hideInputError(formElement, validationConfig);
+    hideInputError(formElement, inputElement, validationConfig);
   }
 }
 
